eb48115b40a416a10d4e34f36f7c32b45cc6b179
Early Ordie <111804246+GrahamBells@users.noreply.github.com>
2024-01-05 22:27:39 +1100

    added bellscribe.py

---
+++ bellscribe.py
@@ -0,0 +1,37 @@
+import os
+import time
+import argparse
+
+# Create the parser
+parser = argparse.ArgumentParser()
+# Add an argument
+parser.add_argument('--address', type=str, required=True)
+parser.add_argument('--order', type=str, required=True)
+parser.add_argument('--fee', type=str, required=True)
+# Parse the argument
+args = parser.parse_args()
+
+location = "/mnt/blockstorage/order-files/" + args.order + "/"
+
+i = 0
+
+os.system("mkdir " + location + "done")
+
+with open('output.csv','w+') as f:
+    for file in sorted(os.listdir(location)):
+            if file != "order_details.txt" and file != "done" and file != "output.csv":
+                    result = 1
+                    i += 1
+                    while result != 0:
+                            print("node . mint " + args.address + " '" + location + file + " '" + args.fee + "'")
+                            result = os.system("node . mint " + args.address + " '" + location + file + " '" + args.fee + "'")
+                            row=','.join([result])+'\n'
+                            f.write(row)
+                            print("Output: " + str(result))
+                            time.sleep(0.5)
+                            if result == 0:
+                                    os.system("mv" + " '" + location + file + "' " + location + "done/")
+                                    print("Count: " + str(i))
+                            if result == 512:
+                                    #skip to next file
+                                    result = 0  
